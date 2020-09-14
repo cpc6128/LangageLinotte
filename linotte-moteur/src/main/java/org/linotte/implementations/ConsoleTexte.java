57d646cb5f94b369f37ca0d64ceb91a96fe075e1
cpc6128 <ronan.mounes@gmail.com>
2020-09-14 11:54:17 +0200

    Travaux sur les accents avec anciennes versions de Windows

--- linotte-moteur/src/main/java/org/linotte/implementations/ConsoleTexte.java
+++ linotte-moteur/src/main/java/org/linotte/implementations/ConsoleTexte.java
@@ -1,7 +1,7 @@
 /***********************************************************************
  * Linotte                                                             *
  * Version release date : October 26, 2005                             *
- * Author : Mounes Ronan ronan.mounes@amstrad.eu                        *
+ * Author : Mounes Ronan ronan.mounes@amstrad.eu                       *
  *                                                                     *
  *     http://langagelinotte.free.fr                                   *
  *                                                                     *
@@ -9,7 +9,7 @@
  * later, for educational and non-commercial purposes only.            *
  * If any part of the code is to be included in a commercial           *
  * software, please contact us first for a clearance at                *
- *   ronan.mounes@amstrad.eu                                            *
+ *   ronan.mounes@amstrad.eu                                           *
  *                                                                     *
  *   This notice must remain intact in all copies of this code.        *
  *   This code is distributed WITHOUT ANY WARRANTY OF ANY KIND.        *
@@ -24,73 +24,81 @@ import org.linotte.moteur.entites.Role;
 import org.linotte.moteur.xml.api.IHM;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
 public class ConsoleTexte implements IHM {
 
-	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    Console console = System.console(); // Mode standard
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// A utiliser si console est null
 
-	static boolean debug = true;
+    static boolean debug = true;
 
-	public boolean debug(String parametre) {
-		if (debug)
-			System.err.println(parametre);
-		return true;
-	}
+    public boolean debug(String parametre) {
+        if (debug)
+            System.err.println(parametre);
+        return true;
+    }
 
-	public String questionne(String message, Role type, String acteur) {
-		afficher(message, type);
-		return demander(type, acteur);
-	}
+    public String questionne(String message, Role type, String acteur) {
+        afficher(message, type);
+        return demander(type, acteur);
+    }
 
-	public String demander(Role type, String acteur) {
-		prompt();
-		String message;
-		try {
-			if (type == Role.NOMBRE) {
-				BigDecimal result = null;
-				do {
-					message = in.readLine();
-					try {
-						result = new BigDecimal(message);
-					} catch (NumberFormatException e1) {
-						System.out.println("!Nombre non valide!");
-						prompt();
-					}
-				} while (result == null);
-				message = result.toString();
-			} else
-				message = in.readLine();
-		} catch (IOException e) {
-			message = "0";
-		} catch (Exception e) {
-			message = "0";
-		}
-		return message;
-	}
+    public String demander(Role type, String acteur) {
+        String message;
+        try {
+            if (type == Role.NOMBRE) {
+                BigDecimal result = null;
+                do {
+                    message = demanderProxy(">"); //in.readLine();
+                    try {
+                        result = new BigDecimal(message);
+                    } catch (NumberFormatException e1) {
+                        ecrireProxy("!Nombre non valide!");
+                    }
+                } while (result == null);
+                message = result.toString();
+            } else
+                message = demanderProxy(">");// in.readLine();
+        } catch (Exception e) {
+            message = "0";
+        }
+        return message;
+    }
 
-	public boolean afficher(String afficher, Role type) {
-		System.out.println(afficher);
-		return true;
-	}
+    public boolean afficher(String afficher, Role type) {
+        ecrireProxy(afficher);
+        return true;
+    }
 
-	public static void setDEBUG(boolean b) {
-		debug = b;
-	}
+    public static void setDEBUG(boolean b) {
+        debug = b;
+    }
 
-	public void prompt() {
-		System.out.print(">");
-	}
+    public boolean effacer() {
+        return false;
+    }
 
-	public boolean effacer() {
-		return false;
-	}
+    public boolean afficherErreur(String afficher) {
+        System.err.println(afficher);
+        return true;
+    }
 
-	public boolean afficherErreur(String afficher) {
-		System.err.println(afficher);
-		return true;
-	}
+    private void ecrireProxy(String afficher) {
+        if (console != null)
+            console.printf(afficher + "\n");
+        else
+            System.out.println(afficher);
+    }
+
+    private String demanderProxy(String prompt) throws IOException {
+        if (console != null)
+            return console.readLine(prompt);
+        else
+            return in.readLine();
+    }
 
 }
