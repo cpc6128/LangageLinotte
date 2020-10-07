763104bf05c98aec90ab26790e8c6d35e0d2e500
cpc6128 <ronan.mounes@gmail.com>
2020-10-07 11:30:32 +0200

    Ne pas afficher les erreurs 3D

--- linotte-greffons/src/main/java/org/linotte/greffons/BibliothequeGreffons.java
+++ linotte-greffons/src/main/java/org/linotte/greffons/BibliothequeGreffons.java
@@ -7,6 +7,7 @@ import org.linotte.greffons.impl.swing.layout.Grille;
 import org.linotte.greffons.impl.xyz.*;
 import org.linotte.moteur.xml.Linotte;
 import org.linotte.moteur.xml.api.Librairie;
+import org.linotte.moteur.xml.Version;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,7 +34,9 @@ public final class BibliothequeGreffons {
         try {
             greffons3D();
         } catch (Throwable e) {
-            e.printStackTrace();
+            if (Version.isBeta()) {
+                e.printStackTrace();
+            }
         }
 
     }
