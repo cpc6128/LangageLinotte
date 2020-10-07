4379cb167af4ba2d107bcb9fba795d308e9777b4
cpc6128 <ronan.mounes@gmail.com>
2020-10-07 11:37:24 +0200

    CHCP de la table de codage en fonction de l'os

--- linotte-accessibilite/src/main/basic/Tablette.bat
+++ linotte-accessibilite/src/main/basic/Tablette.bat
@@ -1,4 +1,15 @@
 @echo off
-chcp 850 >NUL
+
+setlocal
+for /f "tokens=2 delims=[]" %%i in ('ver') do set VERSION=%%i
+for /f "tokens=2-3 delims=. " %%i in ("%VERSION%") do set VERSION=%%i.%%j
+if "%VERSION%" == "5.00" echo chcp 1252 >NUL
+if "%VERSION%" == "5.0" echo chcp 1252 >NUL
+if "%VERSION%" == "5.1" echo chcp 1252 >NUL
+if "%VERSION%" == "5.2" echo chcp 1252 >NUL
+if "%VERSION%" == "6.0" echo chcp 1252 >NUL
+if "%VERSION%" == "6.1" echo chcp 1252 >NUL
+endlocal
+
 title lecture
 java -jar Atelier.jar -a %1
