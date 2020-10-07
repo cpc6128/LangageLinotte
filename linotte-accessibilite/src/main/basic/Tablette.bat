53ebde4913b6628c0c3b9c49aacb54e28c4925e1
ronan <ronan.mounes@gmail.com>
2020-10-07 21:50:15 +0200

    modification pour l'accessibilité : encodage

--- linotte-accessibilite/src/main/basic/Tablette.bat
+++ linotte-accessibilite/src/main/basic/Tablette.bat
@@ -1,15 +1,4 @@
 @echo off
-
-setlocal
-for /f "tokens=2 delims=[]" %%i in ('ver') do set VERSION=%%i
-for /f "tokens=2-3 delims=. " %%i in ("%VERSION%") do set VERSION=%%i.%%j
-if "%VERSION%" == "5.00" echo chcp 1252 >NUL
-if "%VERSION%" == "5.0" echo chcp 1252 >NUL
-if "%VERSION%" == "5.1" echo chcp 1252 >NUL
-if "%VERSION%" == "5.2" echo chcp 1252 >NUL
-if "%VERSION%" == "6.0" echo chcp 1252 >NUL
-if "%VERSION%" == "6.1" echo chcp 1252 >NUL
-endlocal
-
+chcp 1252 >NUL
 title lecture
 java -jar Atelier.jar -a %1
